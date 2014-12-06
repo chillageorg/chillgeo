6adb8099a4438bbce78f505802326c33fc260700
Georg <chillageorg@hispeed.ch>
2014-12-06 23:20:32 +0100

    Menu

--- js/ctrl/CtrlModule.js
+++ js/ctrl/CtrlModule.js
@@ -1,51 +1,43 @@
-function HomeCtrl ( $scope, $log, $routeParams  )
-{
-    $log.log ("HomeCtrl");
+function HomeCtrl($scope, $log, $routeParams) {
+    $log.log("HomeCtrl");
 
     $scope.username = $routeParams.name;
 }
-function AboutCtrl ( $scope, $log, $location  )
-{
-    $log.log ("AboutCtrl");
+function AboutCtrl($scope, $log, $location) {
+    $log.log("AboutCtrl");
 
-    $scope.openContact = function ()
-    {
-        $log.log ( $location );
-        $location.path ( "/contact" );
+    $scope.openContact = function () {
+        $log.log($location);
+        $location.path("/contact");
     }
 
 }
-function ServicesCtrl ( $scope, $log, $location  )
-{
-    $log.log ("ServicesCtrl");
+function ServicesCtrl($scope, $log, $location) {
+    $log.log("ServicesCtrl");
 
-    $scope.openContact = function ()
-    {
-        $log.log ( $location );
-        $location.path ( "/services" );
+    $scope.openContact = function () {
+        $log.log($location);
+        $location.path("/services");
     }
 
 }
-function ProjectsCtrl ( $scope, $log, $location  )
-{
-    $log.log ("ProjectsCtrl");
+function ProjectsCtrl($scope, $log, $location) {
+    $log.log("ProjectsCtrl");
 
-    $scope.openContact = function ()
-    {
-        $log.log ( $location );
-        $location.path ( "/projects" );
+    $scope.openContact = function () {
+        $log.log($location);
+        $location.path("/projects");
     }
 
 }
-function ContactCtrl ( $scope, $log  )
-{
-    $log.log ("ContactCtrl");
+function ContactCtrl($scope, $log) {
+    $log.log("ContactCtrl");
 }
 
 var ctrl = angular.module('app.ctrl', [])
-        .controller ( 'HoneCtrl', HomeCtrl )
-        .controller ( 'AboutCtrl', AboutCtrl )
-        .controller ( 'ServicesCtrl', ServicesCtrl )
-        .controller ( 'ProjectsCtrl', ProjectsCtrl )
-        .controller ( 'ContactCtrl', ContactCtrl )
+        .controller('HoneCtrl', HomeCtrl)
+        .controller('AboutCtrl', AboutCtrl)
+        .controller('ServicesCtrl', ServicesCtrl)
+        .controller('ProjectsCtrl', ProjectsCtrl)
+        .controller('ContactCtrl', ContactCtrl)
     ;
